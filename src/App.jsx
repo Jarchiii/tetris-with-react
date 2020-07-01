8b576f81cf5fdd58fa7f6f7877d0b2eab07f2873
Jon <orejajonathan@gmail.com>
2020-07-01 18:18:44 +0200

    line completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ class App extends Component {
         grid :null,
         gridHeight : 10,
         gridWidth : 8,
-        piece: null
+        piece: null,
+        nbrCleanLine : 0
     }
 
 
@@ -63,25 +64,78 @@ class App extends Component {
 
     mergePieceToGrid = () => {
     const virtualGrid = this.state.grid
+    let nbrCleanLine = this.state.nbrCleanLine
 
      this.state.piece.mergeData.forEach((element) => {
          const [y, x] = element.split(" _ ")
          virtualGrid[y][x] = this.state.piece.color
      })
-        this.setState({grid : virtualGrid , piece : null}, () => {
+
+     let { cleanGrid, nbrLineCompleted} =  this.cleanGrid(virtualGrid)
+     nbrCleanLine += nbrLineCompleted
+        
+     
+     this.setState({grid : cleanGrid , piece : null,  nbrCleanLine: nbrCleanLine}, () => {
+        
             this.generatePiece();
         })
         console.log(this.state.grid)
     }
+
+    cleanGrid = (grid) => {
+        let cleanGrid = []
+        let nbrLineCompleted = 0
+        for (let y = 0 ; y<this.state.gridHeight; y++){
+            let lineCompleted = true
+            for (let x = 0; x <this.state.gridWidth; x++){
+                if (grid[y][x]===0){
+                    lineCompleted = false
+                }
+            }
+            if (lineCompleted===false){
+                cleanGrid.push(grid[y])
+
+            } 
+            }
+            nbrLineCompleted= this.state.gridHeight - cleanGrid.length
+            for (let i = 0; i < nbrLineCompleted; i++){
+                cleanGrid.unshift(this.makeCleanLine(this.state.gridWidth))
+            }
+
+            return { cleanGrid, nbrLineCompleted}
+ 
+        }
+
+        makeCleanLine (width){
+            let line = []
+            for (let x = 0; x <width; x++){
+                line.push(0)
+            }
+            return line
+
+        }
+
+
+    
 
     //PIECE FONCTIONS
     generatePiece = () =>{
         let piece = {}
         piece.posX = 0
         piece.posY = 0
-        piece.grid = pieceCollection[0]
+        let indexPieceCollection = [Math.floor(Math.random() * pieceCollection.length)]
         piece.mergeData = []
-        piece.color = Math.floor(Math.random() *5)+1
+        piece.grid = pieceCollection[indexPieceCollection]
+        piece.color = indexPieceCollection + 1 
+
+        if (piece.grid[0][0] ===0){
+            piece.posY--
+        }
+
+        piece.posX= Math.floor((this.state.gridWidth - piece.grid[0].length)/2)
+
+
+
         let coordinate = this.pieceCanBeMove(piece)
   
         if (coordinate !== false){
@@ -213,6 +267,7 @@ class App extends Component {
     return(
       <div id="wrapper_tetris">
       <h1>Tetris</h1>
+      <p className="score">{this.state.nbrCleanLine}</p>
       {this.state.grid  !== null &&
        <Grid 
        grid={this.state.grid}
